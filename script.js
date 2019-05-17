26a813f4871df31666116b79cbdaddca7a2e605b
Trevor Ramlow <trevorramlow@gmail.com>
2019-05-17 15:40:29 -0500

    setup inputs to receive data and asdd to tasble

--- script.js
+++ script.js
@@ -4,4 +4,25 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('jquery');
+    $('#submitButton').on('click', addEmployee);
+}
+
+function addEmployee() {
+    console.log('submit click');
+
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let id = $('#id').val();
+    let title = $('#titile').val();
+    let annualSalary = $('#annualSalary').val();
+
+    $('#tableBody').append(`
+        <tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${id}</td>
+            <td>${title}</td>
+            <td>${annualSalary}</td>
+        </tr>
+    `)
 }
