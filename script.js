2c2467e302616ad7112a51562e2262fff87a7f5f
Trevor Ramlow <trevorramlow@gmail.com>
2019-05-17 16:37:35 -0500

    added the delete button functionality

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ let total = 0
 
 function readyNow() {
     console.log('jquery');
-    $('#submitButton').on('click', addEmployee);
+    $('#submitButton').on('click', addEmployee); //when the submit button is clicked, run the add employee function
+    $('#tableBody').on('click', '.deleteRow', removeEmployee); //hey jq, when we click in the table body, specifically the money class, run the remove employee function
+
 }
 
 function addEmployee() {
-    console.log('submit click');
+    console.log('submit click'); //each time this function is run, it will conbsole log this
 
     let firstName = $('#firstName').val();
     let lastName = $('#lastName').val();
@@ -18,7 +20,8 @@ function addEmployee() {
     let title = $('#title').val();
     let annualSalary = $('#annualSalary').val();
 
-    $('#tableBody').append(`
+    //each time this function run, it will append the inputs from the above variables into the below tbales
+    $('#tableBody').append(` 
         <tr>
             <td>${firstName}</td>
             <td>${lastName}</td>
@@ -28,11 +31,17 @@ function addEmployee() {
             <td><button class="deleteRow">Delete</button></td>
         </tr>
     `)
-
+    // nbow empty the inputs 
     $('#firstName').val('');
     $('#lastName').val('');
     $('#id').val('');
     $('#title').val('');
     $('#annualSalary').val('');
 
+}
+
+//when we run this function, we will remove the parent of the parent of this (this is the button, the parent is the td, and that parent is the row.)
+function removeEmployee() {
+    console.log('remove');
+    $(this).parent().parent().remove(); 
 }
